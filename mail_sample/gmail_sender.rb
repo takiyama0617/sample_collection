09c5816c9f629d85674da9d6c75dcf80cd960e33
Tsutomu Akiyama <akiyamatsutomu@akiyamatsutomunoMacBook-Pro.local>
2019-08-10 17:23:08 +0900

    Gmailのメール送信のサンプルコード

--- mail_sample/gmail_sender.rb
+++ mail_sample/gmail_sender.rb
@@ -1,2 +1,35 @@
 require 'mail'
 
+def send_mail(params, mail)
+  Mail.defaults do
+    delivery_method :smtp, params
+  end
+
+  puts mail
+  m = Mail.new do
+    from mail[:from]
+    to mail[:to]
+    subject mail[:subject]
+    body mail[:body]
+  end
+
+  m.charset
+  m.deliver
+end
+
+params = {
+  address: 'smtp.gmail.com',
+  port: 587,
+  domain: 'gmail.com',
+  user_name: 'GoogleアカウントID',
+  password: 'Googleアカウントのパスワード'
+}
+
+mail = {
+  from: 'from_hoge@example.com',
+  to: 'to_hoge@example.com',
+  subject: 'test_subject',
+  body: 'test_body'
+}
+
+send_mail(params, mail)
