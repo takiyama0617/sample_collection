6471f5dd8694a2e2bdd73f3844cc8c49deb76ea5
Tsutomu Akiyama <akiyamatsutomu@akiyamatsutomunoMacBook-Pro.local>
2019-08-27 01:10:34 +0900

    一時コミット

--- pdf_sample/pdf_analyzer.rb
+++ pdf_sample/pdf_analyzer.rb
@@ -17,7 +17,22 @@ end
 
 def parse_pdf(file)
   reader = PDF::Reader.new(file)
-  pp reader.methods
+  # pp reader.methods.sort
+  reader.pages.each do |page|
+    # pp page.page_object.methods.sort
+    # pp page.objects.methods.sort
+    page.page_object.each do |obj|
+      puts obj
+    end
+    puts 'PDF TEXT DATA -------------'
+    puts "page num : #{page.number}"
+    text = page.text.empty? ? 'empty' : page.text.slice(0, 10)
+    puts "→ PDF page text : #{text}..."
+
+    File.open("./output/sample_#{page.number}.pdf", 'wb') do |f|
+      f.write(page.text)
+    end
+  end
 end
 
 main if $PROGRAM_NAME == __FILE__
